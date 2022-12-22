a5938d3851fd534a5bfe06111c6ac39e0ac7edbc
AcidCommunism <max.zamota@gmail.com>
2022-12-22 21:31:05 +0400

    wrap test steps/read auth creds from secrets

--- tests/insurance-api.spec.ts
+++ tests/insurance-api.spec.ts
@@ -1,7 +1,4 @@
 import { test, expect } from '@playwright/test';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const httpCredentials = {
     username: process.env.AUTH_USERNAME || 'some@default.user',
@@ -27,41 +24,55 @@ test.describe('order placement tests', () => {
 
     test('should create new order @positive', async ({ request }) => {
         // Get product list
-        const productListResponse = await request.get(`./traveling_abroad`);
-        expect(productListResponse.ok()).toBeTruthy();
-        const productList = await productListResponse.json();
-        expect(productList.data.map(i => i.id).includes(testProductId)).toBe(
-            true
-        );
+        const productList =
+            await test.step('Get list of products', async () => {
+                const productListResponse = await request.get(
+                    `./traveling_abroad`
+                );
+                expect(productListResponse.ok()).toBeTruthy();
+                return productListResponse.json();
+            });
+        test.step(`Check that product list contains item with id ${testProductId}`, async () =>
+            expect(
+                productList.data.map(i => i.id).includes(testProductId)
+            ).toBe(true));
 
         // Get product info
-        const productInfoResponse = await request.get(
-            `./traveling_abroad/${testProductId}`
-        );
-        expect(productInfoResponse.ok()).toBeTruthy();
-        const productInfo = await productInfoResponse.json();
-        expect(productInfo.data.id).toBe(testProductId);
+        const productInfo =
+            await test.step(`Get product info (id: ${testProductId}`, async () => {
+                const productInfoResponse = await request.get(
+                    `./traveling_abroad/${testProductId}`
+                );
+                expect(productInfoResponse.ok()).toBeTruthy();
+                return productInfoResponse.json();
+            });
+        test.step(`Check that correct product is returned (id: ${testProductId})`, async () =>
+            expect(productInfo.data.id).toBe(testProductId));
 
         // Place new order
-        const placeOrderResponse = await request.post(
-            './traveling_abroad_certificate',
-            {
-                data: {
-                    is_available: true,
-                    traveling_abroad: testProductId,
-                },
-            }
-        );
-        expect(placeOrderResponse.ok()).toBeTruthy();
-        const newOrderData = await placeOrderResponse.json();
-        console.log(newOrderData);
+        const newOrderData = await test.step('Create new order', async () => {
+            const placeOrderResponse = await request.post(
+                './traveling_abroad_certificate',
+                {
+                    data: {
+                        is_available: true,
+                        traveling_abroad: testProductId,
+                    },
+                }
+            );
+            expect(placeOrderResponse.ok()).toBeTruthy();
+            return placeOrderResponse.json();
+        });
         const certificateId = newOrderData.data.id || newOrderData.data[0].id;
 
         // Get order data
-        const getOrderResponse = await request.get(
-            `./traveling_abroad_certificate/${certificateId}`
-        );
-        const orderData = await getOrderResponse.json();
+        const orderData =
+            await test.step(`Get order with id: ${certificateId}`, async () => {
+                const getOrderResponse = await request.get(
+                    `./traveling_abroad_certificate/${certificateId}`
+                );
+                return getOrderResponse.json();
+            });
 
         // Update order info
         const today = new Date();
@@ -94,34 +105,45 @@ test.describe('order placement tests', () => {
             start_alien: false,
             traveling_type: 'Однократная',
         };
-        const updateOrderResponse = await request.patch(
-            `./traveling_abroad_certificate/${certificateId}`,
-            {
-                data: patchOrderData,
-            }
-        );
-        expect(updateOrderResponse.ok()).toBeTruthy();
-        const updatedOrder = await updateOrderResponse.json();
-        Object.keys(patchOrderData).forEach(k => {
-            if (k !== 'country') {
-                expect(updatedOrder.data[k]).toBe(patchOrderData[k]);
-            } else {
-                expect(updatedOrder.data[k].map(i => i.id)).toStrictEqual(
-                    patchOrderData[k]
+
+        const updatedOrder =
+            await test.step(`Update order with id ${certificateId}`, async () => {
+                const updateOrderResponse = await request.patch(
+                    `./traveling_abroad_certificate/${certificateId}`,
+                    {
+                        data: patchOrderData,
+                    }
                 );
-            }
+                expect(updateOrderResponse.ok()).toBeTruthy();
+                return updateOrderResponse.json();
+            });
+
+        test.step('Check that order data has been properly updated', async () => {
+            Object.keys(patchOrderData).forEach(k => {
+                if (k !== 'country') {
+                    expect(updatedOrder.data[k]).toBe(patchOrderData[k]);
+                } else {
+                    expect(updatedOrder.data[k].map(i => i.id)).toStrictEqual(
+                        patchOrderData[k]
+                    );
+                }
+            });
         });
 
         // Delete order
-        const deleteOrderResponse = await request.delete(
-            `./traveling_abroad_certificate/${certificateId}`
-        );
-        expect(deleteOrderResponse.ok()).toBeTruthy();
+        test.step(`Delete order with id ${certificateId}`, async () => {
+            const deleteOrderResponse = await request.delete(
+                `./traveling_abroad_certificate/${certificateId}`
+            );
+            expect(deleteOrderResponse.ok()).toBeTruthy();
+        });
 
         // Get order returns 404
-        const getDeletedOrderResponse = await request.get(
-            `./traveling_abroad_certificate/${certificateId}`
-        );
-        expect(getDeletedOrderResponse.status()).toBe(404);
+        test.step(`Should return 404 on GET /traveling_abroad_certificate/${certificateId}`, async () => {
+            const getDeletedOrderResponse = await request.get(
+                `./traveling_abroad_certificate/${certificateId}`
+            );
+            expect(getDeletedOrderResponse.status()).toBe(404);
+        });
     });
 });
